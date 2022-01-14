5407a2c79ec31cd175e73623a9499dd77aee56be
Indi Rinearson <indi.rinearson@gmail.com>
2022-01-13 20:32:15 -0800

    chore: added problems

---
+++ BinaryTrees/nodeDepths.js
@@ -0,0 +1,24 @@
+/*
+    Write a function that takes in a Binary Tree and reuturns
+    the sum of its nodes' depths (the distance between the root
+    and the node). Each node has an integara value, a left
+    child node, and a right child node. Children nodes can either
+    be nodes themselves or null.
+*/
+
+function nodeDepths(root) {}
+
+// This is the class of the input binary tree.
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+/*
+  Big O Complexity
+  Time: 
+  Space: 
+*/
---
+++ Searching/threeLargestNums.js
@@ -0,0 +1,13 @@
+/*
+  Write a function that takes in an array of 3+ integars and, without sorting
+  the input array, returns a sorted array of the three largest integars in the
+  input array. If there are duplciate integars, they should be included.
+*/
+
+function findThreeLargestNumbers(array) {}
+
+/*
+  Big O Complexity
+  Time: 
+  Space: 
+*/
